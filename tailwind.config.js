afd941580b5cde827c0a21512caeb827e414fecc
Matt Herold <mattherold@icloud.com>
2024-11-16 23:51:43 +0100

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{gjs,gts,hbs,html,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+        require('@tailwindcss/typography'),
+
+  ],
+};
