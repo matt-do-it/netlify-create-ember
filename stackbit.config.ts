d093b6e982efc73f342ee6290e4c1515cde544f2
Matt Herold <mattherold@icloud.com>
2024-11-16 22:13:40 +0100

    .

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,11 +1,11 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
     "nodeVersion": "18",
     "ssgName": "custom",
     "devCommand": "yarn ember serve --port={PORT}",
-    "contentSources": [],
     "postInstallCommand": "npm i --no-save @stackbit/types",
     experimental: {
     ssg: {
@@ -13,5 +13,34 @@ export default defineStackbitConfig({
       logPatterns: { up: ["Serving on"] },
       passthrough: []
     }
-  }
+  },
+    contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["public/pages"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "public/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        },
+                {
+          name: "People",
+          type: "data",
+          filePath: "public/data/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+
+      ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/"
+      }
+    })
+  ]
+
 })
